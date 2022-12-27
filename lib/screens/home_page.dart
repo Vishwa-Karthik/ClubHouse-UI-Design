6e5b1c21a33068817159b0afa47ea0a911382895
Vishwa-Karthik <vishwa.prarthana@gmail.com>
2022-12-27 22:12:07 +0530

    buttons

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -60,21 +60,110 @@ class HomePage extends StatelessWidget {
       ),
 
       //todo Body of screen
-      body: ListView(
-        padding:
-            const EdgeInsets.only(bottom: 120, left: 20, right: 20, top: 20),
+      body: Stack(
         children: [
-          //todo One single card of details here
-          const UpComingRooms(upcomingRooms: upcomingRoomsList),
+          //* Cards
+          ListView(
+            padding: const EdgeInsets.only(
+                bottom: 120, left: 20, right: 20, top: 20),
+            children: [
+              //todo One single card of details here
+              const UpComingRooms(upcomingRooms: upcomingRoomsList),
 
-          const SizedBox(
-            height: 12.0,
+              const SizedBox(
+                height: 12.0,
+              ),
+
+              // todo room cards from here
+              ...roomsList.map(
+                (e) => RoomCards(room: e),
+              ),
+            ],
           ),
 
-          // todo room cards from here
-          ...roomsList.map(
-            (e) => RoomCards(room: e),
+          //* bottom glass morhphism effect
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 100,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Theme.of(context).scaffoldBackgroundColor.withOpacity(0.1),
+                    Theme.of(context).scaffoldBackgroundColor,
+                  ],
+                ),
+              ),
+            ),
           ),
+
+          //* Start room
+          Positioned(
+            bottom: 60.0,
+            left: 110,
+            child: Container(
+              padding: const EdgeInsets.all(18),
+              decoration: BoxDecoration(
+                color: Theme.of(context).accentColor,
+                borderRadius: BorderRadius.circular(18.0),
+              ),
+              child: const Text.rich(
+                TextSpan(
+                  children: [
+                    //* + button
+                    WidgetSpan(
+                      child: Icon(
+                        CupertinoIcons.add,
+                        size: 21.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                    TextSpan(
+                      text: 'Start a room',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+          //*  menu icon + dot
+          Positioned(
+              bottom: 60.0,
+              right: 40.0,
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      CupertinoIcons.circle_grid_3x3_fill,
+                      size: 38.0,
+                    ),
+                  ),
+                  Positioned(
+                    right: 2,
+                    bottom: 8,
+                    child: Container(
+                      height: 20.0,
+                      width: 20.0,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Theme.of(context).accentColor,
+                      ),
+                    ),
+                  )
+                ],
+              ))
         ],
       ),
     );
